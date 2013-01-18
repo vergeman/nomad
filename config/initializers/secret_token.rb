6b4ffe058a76226ca7c12e3b48bc5bfabf3d9390
Alan Verga <alan.verga@gmail.com>
2013-01-18 13:17:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nomad::Application.config.secret_token = '679ae324d6391fc60d5f63bfc4d470029a78d7183c9278d03b66cc0f72ee686c4376d34517b36064ddc8f89040b6368bd030c3d7e05e0b9827cc2d033d04743e'
