d5e2b5e44441a7ca63bf91b06ea97c7e1201bb39
Alan Verga <alan.verga@gmail.com>
2013-01-22 17:57:12 -0500

    added exists test for contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,10 +12,20 @@ describe "StaticPages" do
 
 
   describe "GET /about" do
-    it "should ahve content 'StaticPages#about'" do
+    it "should have content 'StaticPages#about'" do
       visit '/about'
       page.should have_content('StaticPages#about')
     end
 
   end
+
+  describe "GET /contact" do
+    it "should have content 'StaticPages#contact'" do
+      visit '/contact'
+      page.should have_content('StaticPages#contact')
+    end
+
+  end
+
+
 end
